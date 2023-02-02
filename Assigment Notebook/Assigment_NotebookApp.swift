4e859c045abbe534a686e545d983f26515f1c02a
Faiz Ali <fali@bsd220.org>
2023-02-02 09:56:55 -0600

    Initial Commit

---
+++ Assigment Notebook/Assigment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assigment_NotebookApp.swift
+//  Assigment Notebook
+//
+//  Created by Faiz Ali on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Assigment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
